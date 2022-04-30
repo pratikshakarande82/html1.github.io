d8d0ce0a0be41bfd82494c0119eae9a63c0fa2c4
pratikshakarande82 <99810757+pratikshakarande82@users.noreply.github.com>
2022-04-30 20:34:58 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" >
+    <title>About us</title>
+  </head>
+  <body>
+  <div class="container">
+<section class="my-5">
+<div>
+  <h2 class="pegaline"> About us</h2>
+</div>
+<div>
+<div class="row">
+  <div class="col-lg-6 col-md-6 col-12">
+   <img src="ab.jpg" alt="" class="container-fluid">
+  </div>
+  <div class="col-lg-6 col-md-6 col-12">
+    <h2 class="color"> SHADES OF CAKE</h2>
+    <h4 class="py-5">
+      Dedicated to serving delicious handmade cupcakes , cakes , cookies , banana pudding and much more since 2020
+      <br>
+      Cakes are indeed a bundle of joy that not only flatters your mouth but also adds taste to every function. Everybody loves them for their flavours, taste, and beauty. You get them in every shape, and every flavour and something more appealing is the characteristics of cakes. What makes a cake good is the baking method involved and all the ingredients used in the process. You can make one at home, or you can order cake online, but you cannot just keep your party going without a cake.
+      <br>
+      OUR FEATURS
+      - eautiful appearance
+      <br>
+      - Sweet and perfect taste
+      <br>
+      - Frosting of cakes 
+      <br>
+      - Flavours available
+      <br>
+      - Perfect baking 
+      <br>
+      You can order online or directly visit shop also .
+      <br>
+      <br>
+      --shades of cake (@pratiksha)
+    </h4>
+    <a href="about.php" class="btn btn-success"> read more</a>
+   </div>
+</div>
+</div>
+
+</section>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
---
+++ gallary.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" > 
+
+    <title>GALLARY OF SHADES OF CAKE</title>
+  </head>
+  <body>
+  <div class="container">
+<section class="my-5">
+<div>
+<h2 class="pegaline"> Cakes gallery </h2>
+<div class="card-group">
+  <div class="card">
+    <img src="strawberry-jam.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Strawberry Cake</h5>
+      <p class="card-text"> Half kg and designs available. <br>only 250 RS.</p>
+      <a class="btn btn-primary" href="order.php" role="button">order</a>
+
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 2 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="Blueberry-cream-cake.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Blueberry Cream Cake</h5>
+      <p class="card-text">Half kg and designs available<br> only 300 RS</p>
+      <a class="btn btn-primary" href="order.php" role="button">order</a>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="pineapple.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Pineapple Birthday Cake</h5>
+      <p class="card-text"> Half kg and designs available T<br>only 250 RS</p>
+      <a class="btn btn-primary" href="order.php" role="button">order</a>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
+</div>
+<div class="card-group">
+  <div class="card">
+    <img src="fruit-cake-new.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Mix Fruits Fresh Cake</h5>
+      <p class="card-text">One kg and designs available. <br>only 500 RS </p>
+      <a class="btn btn-primary" href="order.php" role="button">order</a>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="doll cake.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Doll Birthday Cake</h5>
+      <p class="card-text"> one kg and many more shades available<br>only 500 Rs.</p>
+      <a class="btn btn-primary" href="order.php" role="button">order</a>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="fondant-engagement-cake.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Fondant Engagement Cake</h5>
+      <p class="card-text"> 1.5 KG different colors and flavours available with same design. <br>only 1000 RS</p>
+      <a class="btn btn-primary" href="order.php" role="button">order</a>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
+<div class="card-group">
+  <div class="card">
+    <img src="chocolate cookies.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Chcolate cookies</h5>
+      <p class="card-text">1KG packed and healthy snacks<br> only  200 RS</p>
+      <a class="btn btn-primary" href="order.php" role="button">order</a>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="pestries.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Pestries</h5>
+      <p class="card-text"> 4 flavours available s <br>only 60 RS </p>
+      <a class="btn btn-primary" href="order.php" role="button">order</a>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+  <div class="card">
+    <img src="cupings.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Cup Cake</h5>
+      <p class="card-text">colours and flavours available. T<br> only RS 35</p>
+      <a class="btn btn-primary" href="order.php" role="button">order</a>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</div>
+
+</section>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
